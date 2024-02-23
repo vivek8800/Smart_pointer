58f9bc0875a3f68701e36d20bada72cecfb80f3e
Vivek Kumar <90982992+vivek8800@users.noreply.github.com>
2024-02-24 02:06:13 +0530

    Update STL_shared_ptr.cpp

--- STL_shared_ptr.cpp
+++ STL_shared_ptr.cpp
@@ -2,27 +2,35 @@
 #include<memory>
 using namespace std;
 
-class Base{
-int x;
+class Base
+{
+    int x;
 
 public:
 
-Base(int x):x{x}{}
+    Base(int x):x{x}{}
 
-void setx(){
-    this->x=x+7;
-}
-~Base(){
-    cout<<"Destructor called"<<endl;
-}
+    void setx()
+    {
+        this->x=x+7;
+    }
 
+    ~Base()
+    {
+        cout<<"Destructor called"<<endl;
+    }
 };
 
-int main(){
+int main()
+{
     std::shared_ptr<Base> sp(new Base(10));
+    
     cout<<sp.use_count()<<endl;
+    
     std::shared_ptr<Base> sp1=sp;
+    
     cout<<sp.use_count()<<endl;
+    
     cout<<sp1.use_count()<<endl;
 
     return 0;
